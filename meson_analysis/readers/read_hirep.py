7bc4b2a57c81e31ea5566670d6e5c07ad588d50c
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-04-25 22:03:08 +0100

    get more hirep metadata

--- meson_analysis/readers/read_hirep.py
+++ meson_analysis/readers/read_hirep.py
@@ -9,7 +9,14 @@ import numpy as np
 from ..correlator import CorrelatorEnsemble, Correlator
 
 
+_reps = {"FUN": "fundamental", "SYM": "symmetric", "ASY": "antisymmetric", "ADJ": "adjoint"}
+
+
 def add_metadata(metadata, line_contents):
+    """
+    Parse out possible metadata given on a line,
+    and add it to an ensemble metadata dictionary.
+    """
     if (
         line_contents[0] == "[GEOMETRY][0]Global"
         or line_contents[0] == "[GEOMETRY_INIT][0]Global"
@@ -24,10 +31,44 @@ def add_metadata(metadata, line_contents):
         metadata["NY"] = NY
         metadata["NZ"] = NZ
 
+    if line_contents[0].startswith("[MAIN][0]Mass["):
+        if "valence_masses" not in metadata:
+            metadata["valence_masses"] = []
+        if (valence_mass := float(line_contents[2])) not in metadata["valence_masses"]:
+            metadata["valence_masses"].append(valence_mass)
+
+    if line_contents[:2] == ["[MAIN][0]Fermion", "representation:"]:
+        metadata["valence_representation"] = line_contents[2][5:].lower()
+
+    if line_contents[:2] == ["[MAIN][0]Gauge", "group:"]:
+        group_family, Nc = line_contents[2].strip(")").split("(")
+        metadata["group_family"] = group_family
+        metadata["Nc"] = int(Nc)
+
+
+def add_single_consistent_metadatum(metadata, name, value):
+    if value and name in metadata and metadata[name] != value:
+        logging.warning(f"{name} values are not consistent!")
+    metadata[name] = value
+
+
+def add_cfg_metadata(metadata, Nc, rep, Nf, beta, mass):
+    """
+    Verify and attach metadata from analysed configurations
+    to an ensemble metadata dictionary.
+    """
+    if int(Nc) != metadata.get("Nc"):
+        logging.warning("Configuration Nc does not match valence Nc")
+
+    add_single_consistent_metadatum(metadata, "dynamical_representation", rep)
+    add_single_consistent_metadatum(metadata, "Nf", Nf)
+    add_single_consistent_metadatum(metadata, "beta", beta)
+    add_single_consistent_metadatum(metadata, "dynamical_mass", mass)
+
 
 def parse_cfg_filename(filename):
     """
-    Parse out the run name and trajectory index from a configuration filename.
+    Parse out the run parameters and trajectory index from a configuration filename.
 
     Arguments:
 
@@ -36,15 +77,21 @@ def parse_cfg_filename(filename):
     Returns:
 
         run_name: The name of the run/stream
+        Nc: the number of colors
+        rep: the fermion representation used for dynamical flavors
+        Nf: the number of dynamical fermion flavors
+        beta: the lattice coupling beta used to generate the ensemble
+        mass: the mass of dynamical fermion flavors
         cfg_index: The index of the trajectory in the stream
     """
 
     matched_filename = re.match(
-        r".*/([^/]*)_[0-9]+x[0-9]+x[0-9]+x[0-9]+nc[0-9]+(?:r[A-Z]+)?(?:nf[0-9]+)?b[0-9]+\.[0-9]+m-?[0-9]+\.[0-9]+n([0-9]+)",
+        r".*/([^/]*)_[0-9]+x[0-9]+x[0-9]+x[0-9]+nc([0-9]+)(?:r([A-Z]+))?(?:nf([0-9]+))?b([0-9]+\.[0-9]+)m(-?[0-9]+\.[0-9]+)n([0-9]+)",
         filename,
     )
-    run_name, cfg_index = matched_filename.groups()
-    return run_name, cfg_index
+    run_name, Nc, rep, Nf, beta, mass, cfg_index = matched_filename.groups()
+
+    return run_name, int(Nc), _reps[rep], int(Nf), float(beta), float(mass), int(cfg_index)
 
 
 def add_row(ensemble, split_line, stream_name, cfg_index):
@@ -89,6 +136,10 @@ def add_row(ensemble, split_line, stream_name, cfg_index):
 
 @lru_cache(maxsize=8)
 def read_correlators_hirep(filename):
+    """
+    Read the correlation functions and associated metadata
+    from the file in the specified `filename`.
+    """
     correlators = CorrelatorEnsemble(filename)
 
     # Track which configurations have provided results;
@@ -102,7 +153,8 @@ def read_correlators_hirep(filename):
                 line_contents[0] == "[IO][0]Configuration"
                 and line_contents[2] == "read"
             ):
-                run_name, cfg_index = parse_cfg_filename(line_contents[1][1:-1])
+                run_name, Nc, rep, Nf, beta, mass, cfg_index = parse_cfg_filename(line_contents[1][1:-1])
+                add_cfg_metadata(correlators.metadata, Nc, rep, Nf, beta, mass)
 
                 if (run_name, cfg_index) in read_cfgs:
                     logging.warn(
